95026ab5ec4d4085ad6e11890c9b5f975da7ff5a
Patricia Campbell <pcampbell@dawsoncollege.qc.ca>
2022-01-10 12:13:56 -0500

    initial commit

---
+++ algebraeic-example/algebra-popy.py
@@ -0,0 +1,18 @@
+'''
+simple example calculate
+f(x,y,z)  = x + 2y + 3z
+p.campbell
+2022-01-06
+
+'''
+def f(x,y,z): 
+    print("in f()",x ,y,z)
+    return x + (2*y) + (3*z)
+
+test = [ 5, 6, 10 ]
+# plain old python 
+print("using plain old python: \nresult: {} from x,y,z values  {}".format(f(test[0],test[1],test[2]), test))
+test =  [10, 10, 10]
+print("using plain old python: \nresult: {} from x,y,z values  {}".format(f(test[0],test[1],test[2]), test))
+
+print("using plain old python: \nresult: {} from x,y,z values  {}".format(f(3,2,1), [3,2,1]))
