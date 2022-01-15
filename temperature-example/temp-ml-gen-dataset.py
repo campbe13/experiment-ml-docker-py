73262b40c15e221a319386091d94d9e88d7c7eea
Tricia <pcampbell.edu@gmail.com>
2022-01-14 19:06:37 -0500

    try bigger set to fix the rate problem

--- temperature-example/temp-ml-gen-dataset.py
+++ temperature-example/temp-ml-gen-dataset.py
@@ -23,22 +23,24 @@ import random
 X = list()
 y = list()
 # 100 >=10
-for i in range(1000):
+for i in range(5000):
     # random float 2 decimal points
-    z = round(random.uniform(10.0, 35.0), 2)
+    z = round(random.uniform(10.0, 35.0), 3)
     X.append([z])
     y.append('none')
 # 100 <10
-for i in range(1000):
+for i in range(5000):
     # random float 2 decimal points
-    z = round(random.uniform(-35.0, 9.99), 2)
+    z = round(random.uniform(9.99, -35.0), 3)
     X.append([z])
     y.append('coat')
-
+for i in range(0,9):
+    X.append([i])
+    y.append('coat')
 #print("X ",X)
 #print("y ",y)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20) 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25) 
 scaler = StandardScaler()
 scaler.fit(X_train)
 
@@ -61,11 +63,15 @@ print ("predict ", classifier.predict(test2))
 test2 = [[-25]]
 print ("given ", test2 )
 print ("predict ", classifier.predict(test2))
-for i in range(-10,20):
-    test2 = [[i]]
-    print ("given ", test2, " predict ", classifier.predict(test2))
+for i in range(30):
+    test2 = [[round(random.uniform(-35.0, 35.0), 3)]]
+    result = classifier.predict(test2)
+    #print ("given ", test2, " predict ", result) 
+    if result == 'coat':
+        print(f'When it\'s {test2[0][0]} wear a {result[0]}')
+    else:
+        print(f'When it\'s  {test2[0][0]} wear  {result[0]}')
 
 # Print results: 
-
-print("confusion matrix", confusion_matrix(y_test, y_predict))
-print("classification report\n", classification_report(y_test, y_predict)) 
+#print("confusion matrix", confusion_matrix(y_test, y_predict))
+#print("classification report\n", classification_report(y_test, y_predict)) 
